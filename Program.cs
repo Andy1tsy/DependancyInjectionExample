9a29d144405356943b57472989ec9ceca27a72c2
Andrii Tsyhankov <43074151+Andy1tsy@users.noreply.github.com>
2019-06-20 15:49:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependancyInjectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader1 =  DIConfig.Configure("AdoNet");
+            ExecuteMethod(reader1);
+            reader1.ExecuteStoredProcedure();
+
+            var reader2 = DIConfig.Configure("Dapper");
+            ExecuteMethod(reader2);
+
+            var reader3 = DIConfig.Configure("EF");
+            ExecuteMethod(reader3);
+
+            Console.ReadLine();
+        }
+
+        private static void ExecuteMethod(IDbReader dbReader)
+        {
+            //m.b. some logic
+            dbReader.Read();
+        }
+
+    }
+}
